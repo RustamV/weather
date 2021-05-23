15d40cdfdf47ca6e044fa3234bfeef6b8f7e64bf
RustamV <rvrd1996@gmail.com>
2021-05-23 19:59:44 +0500

    simple fetch

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "antd/dist/antd.css";
 import Weather from "./components/Weather";
 
 ReactDOM.render(<Weather />, document.getElementById("root"));
